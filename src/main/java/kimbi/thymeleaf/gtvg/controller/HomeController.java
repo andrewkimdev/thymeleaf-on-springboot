476c80e293b2cdef3265c2cbc12a82360709b603
Byung-In KIM <kimbi@cliensoft.com>
2025-05-08 17:22:07 +0900

    Implement `/home` and its template (basic)

--- src/main/java/kimbi/thymeleaf/gtvg/controller/HomeController.java
+++ src/main/java/kimbi/thymeleaf/gtvg/controller/HomeController.java
@@ -2,11 +2,21 @@ package kimbi.thymeleaf.gtvg.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Calendar;
 
-@Controller
+@RequestMapping("/")
+@Controller()
 public class HomeController {
+
+  @ModelAttribute("today")
+  private Calendar getToday() {
+    return Calendar.getInstance();
+  }
+
   @GetMapping("")
   public String home() {
-    return "home";
+    return "/home";
   }
 }
