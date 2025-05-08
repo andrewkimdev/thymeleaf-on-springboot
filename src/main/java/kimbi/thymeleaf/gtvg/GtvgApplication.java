6308b9d1671ed37bb6e12cba117831d547b5f67d
Byung-In KIM <kimbi@cliensoft.com>
2025-05-08 14:13:58 +0900

    First commit

---
+++ src/main/java/kimbi/thymeleaf/gtvg/GtvgApplication.java
@@ -0,0 +1,13 @@
+package kimbi.thymeleaf.gtvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtvgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtvgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimbi/thymeleaf/gtvg/GtvgApplicationTests.java
@@ -0,0 +1,13 @@
+package kimbi.thymeleaf.gtvg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtvgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
